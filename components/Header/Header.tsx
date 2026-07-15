6e1cb577068850eecae6b63c7a272fe255954d8b
Kimmimaru <kseniasereda12.01@gmail.com>
2026-07-15 16:28:45 +0300

    style: improve header alignment and responsive navigation

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,10 +5,10 @@ import css from "./Header.module.css";
 export default function Header() {
   return (
     <header className={css.header}>
-      <Link href="/" aria-label="Home">
+      <Link href="/" aria-label="Home" className={css.brandLink}>
         NoteHub
       </Link>
-      <nav aria-label="Main Navigation">
+      <nav aria-label="Main Navigation" className={css.nav}>
         <ul className={css.navigation}>
           <li>
             <Link href="/">Home</Link>
